b4339fd042c08a4a0b6ec79432991ab7a8c2c3f0
ihmtest <g0pala.reddy@yahoo.com>
2016-09-30 00:33:45 -0400

    test
    test

---
+++ src/com/ihm/base/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.ihm.base;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println ( " Hello AWS, this is IHM") ; 
+
+	}
+
+}
